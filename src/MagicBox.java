3c58f612e2a7c15aeadd999ffda8b42f01de3bd6
LiGiR72 <iliya.alabyew@gmail.com>
2022-09-02 15:20:01 +0300

    MagickBox class implemented

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -1,4 +1,33 @@
-public class MagicBox<T> {
-    T[] box;
+import java.util.Random;
 
+public class MagicBox<T> {
+    protected T[] box;
+    protected Random random = new Random();
+    public MagicBox(int capacity){
+        box = (T[]) new Object[capacity];
+    }
+
+    boolean add(T item){
+        for(int i = 0; i < box.length; i++) {
+            if(box[i] != null){
+                continue;
+            }
+            box[i] = item;
+            return true;
+        }
+        return false;
+    }
+    T pick(){
+        int num = 0;
+        int randomInt = random.nextInt(box.length);
+        for (T item: box) {
+            if (item == null){
+                num++;
+            }
+        }
+        if(num != 0){
+            throw new RuntimeException("В коробке есть еще " + num + " свободных ячеек");
+        }
+        return box[randomInt];
+    }
 }
